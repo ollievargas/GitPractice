e78ad9764fd8cea5c77067d879d215506e4d3af3
ollievargas <49657659+ollievargas@users.noreply.github.com>
2019-05-10 23:03:10 -0500

    Add files via upload

---
+++ pythonExercises.py
@@ -0,0 +1,155 @@
+# Exercise 1
+
+# name = input("What is your name?")
+# print("Hello", name)
+
+# Exercise 2
+
+# name = input("What is your name?")
+# print("Hello", name.upper())
+# print ("Your name has " + str(len(name)) + " letters in it! Awesome!")
+
+# Exercise 3
+
+# sent = input("_language_ is fun to learn at _place_!")
+# sent1 = input("What is language?")
+# sent2 = input("What is place?")
+# print(sent, sent1, sent2)
+
+# Exercise 4
+
+# dow = int(input("Day?(0-6)"))
+# if dow == 0:
+#     print("Sunday")
+# elif dow == 1:
+#     print("Monday")
+# elif dow == 2:
+#     print("Tuesday")
+# elif dow == 3:
+#     print("Wednesday")
+# elif dow == 4:
+#     print("Thursday")
+# elif dow == 5:
+#     print("Friday")
+# elif dow == 6:
+#     print("Saturday")
+# print(dow)
+
+# Exercise 5
+
+# dow = int(input("Day?(0-6)"))
+# if dow == 0:
+#     print("Sleep In.")
+# elif dow == 1:
+#     print("Go to work!")
+# elif dow == 2:
+#     print("Go to work!")
+# elif dow == 3:
+#     print("Go to work!")
+# elif dow == 4:
+#     print("Go to work!")
+# elif dow == 5:
+#     print("Go to work!")
+# elif dow == 6:
+#     print("Sleep In.")
+# print(dow)
+
+# Exercise 6
+
+# cel = int(input("Enter a temperature in celcius: "))
+# fah = (cel * 9/5) + 32
+# print(fah,"F")
+
+# Excercise 7
+
+# totalBill = float(input("Total bill amount? "))
+# service = input("Level of service? ")
+# x = totalBill * 0.20
+# y = totalBill * 0.15
+# z = totalBill * 0.10
+# if service == ("good"):
+#     print("Tip amount: $", x )
+#     print("Total amount: $", totalBill + x )
+# elif service == ("fair"):
+#     print("Tip amount: $", y )
+#     print("Total amount: $", totalBill + y )
+# elif service == ("bad"):
+#     print("Tip amount: $", z )
+#     print("Total amount: $", totalBill + z )
+
+# Exercise 8
+
+# totalBill = float(input("Total bill amount? "))
+# service = input("Level of service? ")
+# checkSplit = int(input("Split how many ways?: "))
+# tip = float(input("Tip amount: $"))
+# x = totalBill + tip
+# print("Total amount: $", totalBill + tip )
+# print("Amount per person: $", x / checkSplit)
+
+# Exercise 9
+
+# x = 1
+# while (x < 11):
+#     print(x)
+#     x += 1
+
+# # Exercise 10
+
+# coins = 0
+# more = "yes"
+# less = "no"
+# statment = ("You have 0 coins.")
+# print(statment)
+# print(input("Do you want another coin? "))
+# while more == "yes":
+#     coins = coins + 1
+#     print("You now have " + str(coins) + " coins.")
+#     more = input("Do you want another? ")
+#     if more == "no":
+#         print("Bye.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
