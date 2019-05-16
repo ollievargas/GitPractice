f5e705f6bf237e5b4a4a085d7e53a5395295d55f
ollievargas <49657659+ollievargas@users.noreply.github.com>
2019-05-16 13:21:12 -0500

    Add files via upload

---
+++ pythonExercises5.py
@@ -0,0 +1,32 @@
+
+#1.) Hello.
+
+# name = input('Whats your name? ')
+# print("Hello", name)
+
+#2.) y = x + 1
+
+# import matplotlib.pyplot as plot 
+
+# def f(x):
+#     return x + 1
+# xs = list(range(-3, 4)) 
+# ys = [] 
+# for x in xs:
+#     ys.append(f(x)) 
+
+# plot.plot(xs, ys) 
+# plot.show()
+
+#3.) Square of x.
+
+import matplotlib.pyplot as plt 
+
+def f(x):
+    # x = x + 1
+    return x
+
+x = [100, -100, 100]
+# y = [0.5]
+plt.plot(x)
+plt.show()
